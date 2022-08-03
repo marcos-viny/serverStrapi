ea8f7c613b2ea35cb9d9f44ba0f02a130a753685
Marcos Vinicius Silva <marcosviniciusmoreira00@gmail.com>
2022-08-03 17:08:21 -0300

    atualização do server

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '767188168f34ab7d53de4388613db246'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '12a7f9f7-1c85-4658-83bb-b78066aacf4d'
+};

33e5bbab45c83823df0a8a2244adb20d5b941e2f
Marcos Vinicius Silva <marcosviniciusmoreira00@gmail.com>
2022-08-03 16:24:18 -0300

    removendo error

--- app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '865a34c55c41ac16012e1dd7dc9d7274'),
-    },
-  },
-});
